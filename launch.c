d2e46c04f5f198b5052db4059689e02595d8ef22
Jonah Marwan <jonahjojo@DESKTOP-4FGKO2I>
2024-07-14 04:14:45 +0300

    Additional Files
    Added Kernel, User-Space Initializer, and bootloader

---
+++ launch.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bootloader/loader.h"
+#include "kernel/kernel.h"
+
+int main(){
+	printf("Hello, Test!");
+	return 0;
+}
---
+++ launch.sh
@@ -0,0 +1,2 @@
+gcc launch.c -o a
+./a
